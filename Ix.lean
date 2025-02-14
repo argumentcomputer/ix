49bc8c58b070fe967cad4f06a67036451d0276ab
Arthur Paulino <arthurleonardo.ap@gmail.com>
2025-02-13 21:00:18 -0300

    feat: a ZK Voting system app (#20)
    Initial version of a ZK Voting system application built with Ix.
    We're still pretty far from making it work, but it can guide the UX development.

--- Ix.lean
+++ Ix.lean
@@ -1,4 +1,6 @@
 -- This module serves as the root of the `Ix` library.
 -- Import modules here that should be built as part of the library.
+import Ix.BuiltIn
+import Ix.Command
 import Ix.Ixon
 import Ix.Prove
