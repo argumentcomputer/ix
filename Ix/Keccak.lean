25d4de5c44a0b11eb2fffbaaea71a5804a5b9685
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2025-03-31 10:27:18 -0400

    Add Lean `hash` function

--- Ix/Keccak.lean
+++ Ix/Keccak.lean
@@ -17,6 +17,11 @@ opaque update : (hasher: Hasher) → (input: @& ByteArray) → Hasher
 @[extern "c_rs_keccak256_hasher_finalize"]
 opaque finalize : (hasher: Hasher) → ByteArray
 
+def hash (input : @& ByteArray) : ByteArray :=
+  let hasher := Hasher.init ()
+  let hasher := hasher.update input
+  hasher.finalize
+
 end Hasher
 
 end Keccak
