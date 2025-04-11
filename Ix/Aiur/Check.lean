afea4bad6ee813a96a159c28c422e449582c1ab6
Arthur Paulino <arthurleonardo.ap@gmail.com>
2025-04-10 21:12:03 -0300

    feat: Aiur types and typechecking
    This patch also introduces the metaprogramming layer for a first
    version of the Aiur DSL, mimicking the Rust syntax.

---
+++ Ix/Aiur/Check.lean
@@ -0,0 +1,377 @@
+import Ix.Aiur.Term
+import Std.Data.HashSet
+
+namespace Aiur
+
+inductive CheckError
+  | duplicatedDefinition : Global → CheckError
+  | undefinedGlobal : Global → CheckError
+  | unboundVariable : Global → CheckError
+  | notAConstructor : Global → CheckError
+  | notAValue : Global → CheckError
+  | notAFunction : Global → CheckError
+  | cannotApply : Global → CheckError
+  | notADataType : Global → CheckError
+  | typeMismatch : Typ → Typ → CheckError
+  | illegalReturn : CheckError
+  | nonNumeric : Typ → CheckError
+  | wrongNumArgs : Global → Nat → Nat → CheckError
+  | notATuple : Typ → CheckError
+  | indexOoB : Nat → CheckError
+  | negativeRange : Nat → Nat → CheckError
+  | rangeOoB : Nat → Nat → CheckError
+  | incompatiblePattern : Pattern → Typ → CheckError
+  | differentBindings : List (Local × Typ) → List (Local × Typ) → CheckError
+  | emptyMatch
+  | branchMismatch : Typ → Typ → CheckError
+  | notAPointer : Typ → CheckError
+  | duplicatedBind : Pattern → CheckError
+
+/--
+Constructs a map of declarations from a toplevel, ensuring that there are no duplicate names
+for functions and datatypes.
+-/
+def Toplevel.mkDecls (toplevel : Toplevel) : Except CheckError Decls := do
+  let map ← toplevel.functions.foldlM (init := default)
+    fun acc function => addDecl acc Function.name .function function
+  toplevel.dataTypes.foldlM (init := map) addDataType
+where
+  ensureUnique name (map : Std.HashMap Global _) := do
+    if map.contains name then throw $ .duplicatedDefinition name
+  addDecl {α : Type} map (nameFn : α → Global) (wrapper : α → Declaration) (inner : α) := do
+    ensureUnique (nameFn inner) map
+    pure $ map.insert (nameFn inner) (wrapper inner)
+  addDataType map dataType := do
+    let dataTypeName := dataType.name
+    ensureUnique dataTypeName map
+    let map' := map.insert dataTypeName (.dataType dataType)
+    dataType.constructors.foldlM (init := map') fun acc (constructor : Constructor) =>
+      addDecl acc (dataTypeName.pushNamespace ∘ Constructor.nameHead) (.constructor dataType) constructor
+
+structure CheckContext where
+  decls : Decls
+  varTypes : Std.HashMap Local Typ
+  returnType : Typ
+
+inductive InferredType
+  /-- A term evaluates to a value of a specific type. -/
+  | evaluates : Typ → InferredType
+  /-- A term causes control flow to escape (e.g., via 'return'). -/
+  | escapes : InferredType
+  deriving Inhabited
+
+abbrev CheckM := ReaderT CheckContext (Except CheckError)
+
+/-- Retrieves the type of a global reference. -/
+def refLookup (global : Global) : CheckM Typ := do
+  let ctx ← read
+  match ctx.decls[global]? with
+  | some (.function function) =>
+    pure $ .function (function.inputs.map Prod.snd) function.output
+  | some (.constructor dataType constructor) =>
+    let args := constructor.argTypes
+    unless args.isEmpty do (throw $ .wrongNumArgs global args.length 0)
+    pure $ .dataType $ dataType.name
+  | some _ => throw $ .notAValue global
+  | none => throw $ .unboundVariable global
+
+def Primitive.type : Primitive → PrimitiveType
+  | u1 _ => .u1
+  | u8 _ => .u8
+  | u16 _ => .u16
+  | u32 _ => .u32
+  | u64 _ => .u64
+
+/-- Extend context with locally bound variables. -/
+def bindIdents (bindings : List (Local × Typ)) (ctx : CheckContext) : CheckContext :=
+  { ctx with varTypes := ctx.varTypes.insertMany bindings }
+
+mutual
+
+partial def inferTerm : Term → CheckM InferredType
+  | .var x => do
+    -- Retrieves and returns the variable type from the context.
+    let ctx ← read
+    match ctx.varTypes[x]? with
+    | some t => pure $ .evaluates t
+    | none =>
+      let Local.str localName := x | unreachable!
+      .evaluates <$> refLookup (Global.init localName)
+  | .ref x => .evaluates <$> refLookup x
+  | .ret term => do
+    -- Ensures that the type of the returned term matches the expected return type.
+    -- The term is not allowed to have a (nested) return.
+    -- Returning the type of the term is not necessary because it's already in the context.
+    let ctx ← read
+    checkNoEscape term ctx.returnType
+    pure .escapes
+  | .data data => .evaluates <$> inferData data
+  | .let pat expr body => do
+    -- Returns the type of the body, inferred in the context extended with the bound variable type.
+    -- The bound variable is ensured not to escape.
+    let typ ← inferNoEscape expr
+    let bindings ← checkPattern pat typ
+    withReader (bindIdents bindings) (inferTerm body)
+  | .match term branches => inferMatch term branches
+  | .if b t f => do
+    -- Ensures that the type of the conditional is a number and the type of both branches are equal,
+    -- unless one of them escapes.
+    -- The returned type is the type of the branches.
+    let _ ← inferNumber b
+    let inferredT ← inferTerm t
+    let inferredF ← inferTerm f
+    match (inferredT, inferredF) with
+    | (.evaluates typT, .evaluates typF) => do
+      unless typT == typF do throw $ .typeMismatch typT typF
+      pure inferredT
+    | (.evaluates _, .escapes) => pure inferredT
+    | _ => pure inferredF
+  | .app func@(⟨.str .anonymous unqualifiedFunc⟩) args => do
+    -- Ensures the function exists in the context and that the arguments, which aren't allowed to
+    -- escape, match the function's input types. Returns the function's output type.
+    let ctx ← read
+    match ctx.varTypes[Local.str unqualifiedFunc]? with
+    | some (.function inputs output) => do
+      checkArgsAndInputs func args inputs
+      pure $ .evaluates output
+    | some _ => throw $ .notAFunction func
+    | none => match ctx.decls[func]? with
+      | some (.function function) => do
+        checkArgsAndInputs func args (function.inputs.map Prod.snd)
+        pure $ .evaluates function.output
+      | some (.constructor dataType constr) => do
+        checkArgsAndInputs func args constr.argTypes
+        pure $ .evaluates (.dataType dataType.name)
+      | _ => throw $ .cannotApply func
+  | .app func args => do
+    -- Only checks global map if it is not unqualified
+    let ctx ← read
+    match ctx.decls[func]? with
+    | some (.function function) =>
+      checkArgsAndInputs func args (function.inputs.map Prod.snd)
+      pure $ .evaluates function.output
+    | some (.constructor dataType constr) => do
+      checkArgsAndInputs func args constr.argTypes
+      pure $ .evaluates $ .dataType dataType.name
+    | _ => throw $ .cannotApply func
+  | .preimg func arg => do
+    -- Checks if the type of the argument, which isn't allowed to escape, matches the
+    -- output type of the function, and infers the type of the function's inputs as a tuple.
+    -- Errors if the function isn't found in the context.
+    let ctx ← read
+    match ctx.decls[func]? with
+    | some (.function function) => do
+      checkNoEscape arg function.output
+      let inpTyp := function.inputs.map Prod.snd
+      pure $ .evaluates $ .tuple inpTyp
+    | some _ => throw $ .notAFunction func
+    | _ => throw $ .unboundVariable func
+  | .xor a b => checkNumBinOp a b
+  | .and a b => checkNumBinOp a b
+  | .inv a => .evaluates <$> inferNumber a
+  | .get tup i => do
+    -- Retrieves the type of an element in a tuple by its index. Errors if the index is out of bounds.
+    let typs ← inferTuple tup
+    if h : i < typs.length then
+      let typ := typs[i]
+      pure $ .evaluates typ
+    else
+      throw $ .indexOoB i
+  | .slice tup i j =>
+    -- Retrieves the types of elements in a tuple by a range (checked to be non-negative) and
+    -- returns them encoded in a `Typ.tuple`. Errors if the index is out of bounds.
+    if j < i then throw $ .negativeRange i j else do
+    let typs ← inferTuple tup
+    if i < typs.length then
+      let slice := typs.drop i |>.take (j - i)
+      pure $ .evaluates $ .tuple slice
+    else
+      throw $ .rangeOoB i j
+  | .store term => do
+    -- Infers the type of the term and returns it, wrapped by a pointer type.
+    -- The term is not allowed to early return.
+    let typ ← inferNoEscape term
+    pure $ .evaluates $ .pointer typ
+  | .load term => do
+    -- Ensures that the the type of the term is a pointer type and returns the unwrapped type.
+    -- The term is not allowed to early return.
+    let typ ← inferNoEscape term
+    match typ with
+    | .pointer innerTyp => pure $ .evaluates innerTyp
+    | _ => throw $ .notAPointer typ
+  | .pointerAsU64 term => do
+    -- Infers the type of the term, which must be a pointer, but returns `.u64`, as in a cast.
+    -- The term is not allowed to early return.
+    let typ ← inferNoEscape term
+    match typ with
+    | .pointer _ => pure $ .evaluates (.primitive .u64)
+    | _ => throw $ .notAPointer typ
+  | .ann typ term => do
+    checkNoEscape term typ
+    pure $ .evaluates typ
+where
+  /--
+  Ensures that there are as many arguments and as expected types and that
+  the types of the arguments are precisely those expected.
+  -/
+  checkArgsAndInputs func args inputs := do
+    let lenArgs := args.length
+    let lenInputs := inputs.length
+    unless lenArgs != lenInputs do throw $ .wrongNumArgs func lenArgs lenInputs
+    args.zip inputs |>.forM fun (arg, input) => checkNoEscape arg input
+  /-- Ensures both arguments are numbers of the same type and returns it. -/
+  checkNumBinOp a b := do
+    let t ← inferNumber a
+    checkNoEscape b t
+    pure $ .evaluates t
+
+partial def checkNoEscape (term : Term) (typ : Typ) : CheckM Unit := do
+  let typ' ← inferNoEscape term
+  unless typ != typ' do throw $ .typeMismatch typ typ'
+
+partial def inferNoEscape (term : Term) : CheckM Typ := do
+  match ← inferTerm term with
+  | .escapes => throw .illegalReturn
+  | .evaluates type => pure type
+
+partial def inferData : Data → CheckM Typ
+  | .primitive prim => pure $ .primitive prim.type
+  | .tuple terms => .tuple <$> terms.mapM inferNoEscape
+
+/-- Checks that a pattern matches a given type and collects its bindings. -/
+partial def checkPattern (pat : Pattern) (typ : Typ) : CheckM $ List (Local × Typ) := do
+  let binds ← aux pat typ
+  let locals := binds.map Prod.fst
+  unless (locals == locals.eraseDups) do throw $ .duplicatedBind pat
+  pure binds
+where
+  aux pat typ := match (pat, typ) with
+    | (.var var, _) => pure [(var, typ)]
+    | (.wildcard, _)
+    | (.primitive (.u1 _), .primitive .u1)
+    | (.primitive (.u8 _), .primitive .u8)
+    | (.primitive (.u16 _), .primitive .u16)
+    | (.primitive (.u32 _), .primitive .u32)
+    | (.primitive (.u64 _), .primitive .u64) => pure []
+    | (.tuple pats, .tuple typs) => do
+      unless pats.length != typs.length do throw $ .incompatiblePattern pat typ
+      pats.zip typs |>.foldlM (init := []) fun acc (pat, typ) => acc.append <$> aux pat typ
+    | (.ref funcName [], typ@(.function ..)) => do
+      let ctx ← read
+      let some (.function function) := ctx.decls[funcName]? | throw $ .incompatiblePattern pat typ
+      let typ' := .function (function.inputs.map Prod.snd) function.output
+      unless typ == typ' do throw $ .typeMismatch typ typ'
+      pure []
+    | (.ref constrRef pats, .dataType dataTypeRef) => do
+      let ctx ← read
+      let some (.dataType dataType) := ctx.decls[dataTypeRef]? | unreachable!
+      let some (.constructor dataType' constr) := ctx.decls[constrRef]? | throw $ .notAConstructor constrRef
+      unless dataType == dataType' do throw $ .incompatiblePattern pat typ
+      let typs := constr.argTypes
+      let lenPats := pats.length
+      let lenTyps := typs.length
+      unless lenPats == lenTyps do throw $ .wrongNumArgs constrRef lenPats lenTyps
+      pats.zip typs |>.foldlM (init := []) fun acc (pat, typ) => acc.append <$> aux pat typ
+    | (.or pat pat', _) => do
+      let bind ← aux pat typ
+      let bind' ← aux pat' typ
+      if bind != bind' then throw $ .differentBindings bind bind' else pure bind
+    | _ => throw $ .incompatiblePattern pat typ
+
+/-- Infers the type of a 'match' expression and ensures its patterns and branches are valid. -/
+partial def inferMatch (term : Term) (branches : List (Pattern × Term)) : CheckM InferredType := do
+  if branches.isEmpty then throw .emptyMatch
+  let patTyp ← inferNoEscape term
+  branches.foldlM (init := .escapes) (checkBranch patTyp)
+where
+  checkBranch patTyp acc branchData := do
+  let (pat, branch) := branchData
+  let bindings ← checkPattern pat patTyp
+  withReader (bindIdents bindings) $ match acc with
+    | .escapes => inferTerm branch -- All branches have escaped so far.
+    | .evaluates matchTyp => do
+      -- Some branch didn't escape, so if this branch doesn't escape it must have the same type
+      -- as the previous non-escaping branch.
+      match ← inferTerm branch with
+      | .escapes => pure acc -- This branch escapes, so just move on.
+      | .evaluates branchTyp =>
+        -- This branch doesn't escape so its type must match the type of the previous non-escaping branch.
+        unless (matchTyp == branchTyp) do throw $ .branchMismatch matchTyp branchTyp
+        pure acc
+
+partial def inferNumber (term : Term) : CheckM Typ := do
+  let typ ← inferNoEscape term
+  match typ with
+  | .primitive .u1
+  | .primitive .u8
+  | .primitive .u16
+  | .primitive .u32
+  | .primitive .u64 => pure typ
+  | _ => throw $ .nonNumeric typ
+
+partial def inferTuple (term : Term) : CheckM (List Typ) := do
+  let typ ← inferNoEscape term
+  match typ with
+  | .tuple typs => pure typs
+  | _ => throw $ .notATuple typ
+
+end
+
+/-- Checks a function to ensure its body's type matches its declared output type. -/
+def checkFunction (function : Function) : CheckM Unit := do
+  if let .evaluates typ ← inferTerm function.body then
+    unless typ == function.output do throw $ .typeMismatch typ function.output
+
+def getFunctionContext (function : Function) (decls : Decls) : CheckContext :=
+  {
+    decls,
+    varTypes := .ofList function.inputs
+    returnType := function.output
+  }
+
+/--
+Ensures that all declarations are wellformed by checking that every datatype reference
+points to an actual datatype in the toplevel.
+
+Note: it's assumed that all constructor declarations are properly extracted from the
+original datatypes.
+-/
+partial def wellFormedDecls (decls : Decls) : Except CheckError Unit := do
+  let mut visited := default
+  for (_, decl) in decls do
+    match EStateM.run (wellFormedDecl decl) visited with
+    | .error e _ => throw e
+    | .ok () visited' => visited := visited'
+where
+  wellFormedDecl : Declaration → EStateM CheckError (Std.HashSet Global) Unit
+    | .dataType dataType => do
+      let map ← get
+      if !map.contains dataType.name then
+        set $ map.insert dataType.name
+        dataType.constructors.flatMap (·.argTypes) |>.forM wellFormedType
+    | .function function => do
+      wellFormedType function.output
+      function.inputs.forM fun (_, typ) => wellFormedType typ
+    -- No need to check constructors because they come from datatype declarations.
+    | .constructor .. => pure ()
+  wellFormedType : Typ → EStateM CheckError (Std.HashSet Global) Unit
+    | .tuple typs => typs.forM wellFormedType
+    | .pointer pointerTyp => wellFormedType pointerTyp
+    | .dataType dataTypeRef => match decls[dataTypeRef]? with
+      | some (.dataType _) => pure ()
+      | some _ => throw $ .notADataType dataTypeRef
+      | none => throw $ .undefinedGlobal dataTypeRef
+    | _ => pure ()
+
+/--
+Checks all top-level definitions in a program, ensuring they are valid and return the map
+of declarations.
+-/
+def checkToplevel (toplevel : Toplevel) : Except CheckError Decls := do
+  let decls ← toplevel.mkDecls
+  wellFormedDecls decls
+  toplevel.functions.forM fun function =>
+    (checkFunction function) (getFunctionContext function decls)
+  pure decls
+
+end Aiur
---
+++ Ix/Aiur/Term.lean
@@ -0,0 +1,110 @@
+import Std.Data.HashMap
+
+namespace Aiur
+
+inductive Local
+  | str : String → Local
+  | idx : Nat → Local
+  deriving BEq, Hashable
+
+structure Global where
+  toName : Lean.Name
+  deriving BEq, Hashable
+
+def Global.init (limb : String) : Global :=
+  ⟨.mkSimple limb⟩
+
+def Global.pushNamespace (global : Global) (limb : String) : Global :=
+  ⟨global.toName.mkStr limb⟩
+
+def Global.popNamespace (global : Global) : Option (String × Global) :=
+  match global.toName with
+  | .str tail head => some (head, ⟨tail⟩)
+  | _ => none
+
+inductive Primitive
+  | u1 : Bool → Primitive
+  | u8 : UInt8 → Primitive
+  | u16 : UInt16 → Primitive
+  | u32 : UInt32 → Primitive
+  | u64 : UInt64 → Primitive
+  deriving BEq, Hashable
+
+inductive Pattern
+  | var : Local → Pattern
+  | wildcard : Pattern
+  | ref : Global → List Pattern → Pattern
+  | primitive : Primitive → Pattern
+  | tuple : List Pattern → Pattern
+  | or : Pattern → Pattern → Pattern
+  deriving BEq, Hashable
+
+inductive PrimitiveType
+  | u1 | u8 | u16 | u32 | u64
+  deriving BEq, Hashable
+
+inductive Typ where
+  | primitive : PrimitiveType → Typ
+  | tuple : List Typ → Typ
+  | pointer : Typ → Typ
+  | dataType : Global → Typ
+  | function : List Typ → Typ → Typ
+  deriving BEq, Hashable, Inhabited
+
+mutual
+
+inductive Term
+  | var : Local → Term
+  | ref : Global → Term
+  | data : Data → Term
+  | ret : Term → Term
+  | «let» : Pattern → Term → Term → Term
+  | «match» : Term → List (Pattern × Term) → Term
+  | «if» : Term → Term → Term → Term
+  | app : Global → List Term → Term
+  | preimg : Global → Term → Term
+  | xor : Term → Term → Term
+  | and : Term → Term → Term
+  | inv : Term → Term
+  | get : Term → Nat → Term
+  | slice : Term → Nat → Nat → Term
+  | store : Term → Term
+  | load : Term → Term
+  | pointerAsU64 : Term → Term
+  | ann : Typ → Term → Term
+  deriving BEq, Hashable
+
+inductive Data
+  | primitive : Primitive → Data
+  | tuple : List Term → Data
+
+end
+
+structure Constructor where
+  nameHead : String
+  argTypes : List Typ
+  deriving BEq
+
+structure DataType where
+  name : Global
+  constructors : List Constructor
+  deriving BEq
+
+structure Function where
+  name : Global
+  inputs : List (Local × Typ)
+  output : Typ
+  body : Term
+
+structure Toplevel where
+  dataTypes : List DataType
+  functions : List Function
+
+inductive Declaration
+  | function : Function → Declaration
+  | dataType : DataType → Declaration
+  | constructor : DataType → Constructor → Declaration
+
+abbrev Decls := Std.HashMap Global Declaration
+
+end Aiur
