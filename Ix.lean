685f5652f30a49a7f0ff6bd3bcaf1ef048fbb8ea
John C. Burnham <john@agathic.com>
2025-04-13 21:51:18 -0400

    fixup merge 2

--- Ix.lean
+++ Ix.lean
@@ -1,6 +1,5 @@
 -- This module serves as the root of the `Ix` library.
 -- Import modules here that should be built as part of the library.
-import Ix.BuiltIn
 import Ix.Ixon
 import Ix.TransportM
 import Ix.IR
