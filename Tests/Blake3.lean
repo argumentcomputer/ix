95149016f0cf227f40d5951148298318d0347068
Artem Storozhuk <storojs72@gmail.com>
2025-06-05 16:33:45 +0300

    chore: Use Arrays for internal computations during compression

--- Tests/Blake3.lean
+++ Tests/Blake3.lean
@@ -53,7 +53,7 @@ def D : Vector (Fin 32) 8 := #v[12, 13, 14, 15, 15, 12, 13, 14]
 def MX : Vector (Fin 32) 8 := #v[16, 18, 20, 22, 24, 26, 28, 30]
 def MY : Vector (Fin 32) 8 := #v[17, 19, 21, 23, 25, 27, 29, 31]
 
-def permute (state: Vector UInt32 32) : Vector UInt32 32 :=
+def permute (state: Array UInt32) : Array UInt32 :=
   let left := state.extract 0 16
   let right := state.extract 16 32
   let permuted := right.mapIdx (fun i _ =>
@@ -63,13 +63,13 @@ def permute (state: Vector UInt32 32) : Vector UInt32 32 :=
 
   left ++ permuted
 
-def transition (state: Vector UInt32 32) (j : Fin 8) : Vector UInt32 32 :=
-  let aIn := state[(A[j])]
-  let bIn := state[(B[j])]
-  let cIn := state[(C[j])]
-  let dIn := state[(D[j])]
-  let mxIn := state[(MX[j])]
-  let myIn := state[(MY[j])]
+def transition (state: Array UInt32) (j : Fin 8) : Array UInt32 :=
+  let aIn := state[(A[j])]!
+  let bIn := state[(B[j])]!
+  let cIn := state[(C[j])]!
+  let dIn := state[(D[j])]!
+  let mxIn := state[(MX[j])]!
+  let myIn := state[(MY[j])]!
 
   let a0 := aIn.add (bIn.add mxIn)
   let d0 := UInt32.ofBitVec ((dIn.xor a0).toBitVec.rotateRight 16)
@@ -81,15 +81,15 @@ def transition (state: Vector UInt32 32) (j : Fin 8) : Vector UInt32 32 :=
   let c1 := c0.add d1
   let b1 := UInt32.ofBitVec ((b0.xor c1).toBitVec.rotateRight 7)
 
-  let state := state.set (A[j]).toNat a1
-  let state := state.set (B[j]).toNat b1
-  let state := state.set (C[j]).toNat c1
-  let state := state.set (D[j]).toNat d1
+  let state := state.modify (A[j]).toNat fun _ => a1
+  let state := state.modify (B[j]).toNat fun _ => b1
+  let state := state.modify (C[j]).toNat fun _ => c1
+  let state := state.modify (D[j]).toNat fun _ => d1
 
   state
 
 
-def roundNoPermute(state: Vector UInt32 32) : Vector UInt32 32 :=
+def roundNoPermute(state: Array UInt32) : Array UInt32 :=
   let state := transition state 0
   let state := transition state 1
   let state := transition state 2
@@ -100,16 +100,16 @@ def roundNoPermute(state: Vector UInt32 32) : Vector UInt32 32 :=
   let state := transition state 7
   state
 
-def round (state: Vector UInt32 32) : Vector UInt32 32 :=
+def round (state: Array UInt32) : Array UInt32 :=
   let state := roundNoPermute state
   permute state
 
 
-def compress (cv : Vector UInt32 8) (blockWords : Vector UInt32 16) (counter : UInt64) (blockLen flags : UInt32) : Vector UInt32 16 :=
+def compress (cv : Vector UInt32 8) (blockWords : Vector UInt32 16) (counter : UInt64) (blockLen flags : UInt32) : Array UInt32 :=
   let counterLow := UInt32.ofBitVec (counter.toBitVec.truncate 32)
   let counterHigh := UInt32.ofBitVec ((counter.shiftRight 32).toBitVec.truncate 32)
 
-  let state := cv ++ (IV.extract 0 4) ++ #v[counterLow, counterHigh, blockLen, flags] ++ blockWords
+  let state := cv.toArray ++ (IV.extract 0 4).toArray ++ #[counterLow, counterHigh, blockLen, flags] ++ blockWords.toArray
 
   let state := round state
   let state := round state
@@ -121,7 +121,7 @@ def compress (cv : Vector UInt32 8) (blockWords : Vector UInt32 16) (counter : U
 
   let temp := ((state.extract 0 8).zipWith (Xor.xor) (state.extract 8 16))
   let state := temp.append (state.extract 8 32)
-  let temp := (state.extract 8 16).zipWith (Xor.xor) cv
+  let temp := (state.extract 8 16).zipWith (Xor.xor) cv.toArray
   let state := state.extract 0 8 ++ temp ++ state.extract 16 32
 
   state.extract 0 16
@@ -140,7 +140,7 @@ def Tests.Blake3.suite : List LSpec.TestSeq :=
     let counter : UInt64 := 0xbbbbbbbbcccccccc
     let blockLen : UInt32 := 0xeeeeeeee
     let flags : UInt32 := 0xdddddddd
-    let expected : Vector UInt32 16 := #v[0x8980fe15, 0x55898ce0, 0x8cf4fbde, 0x5e8537e9, 0x3d2e54f, 0x7e46753f, 0x5d151bb8, 0x2559b733, 0x24560929, 0x6625b1bf, 0xaaccc80e, 0xc5d4287a, 0x2848c46b, 0x94f666c, 0x3adaaeb3, 0x12011250]
+    let expected : Array UInt32 := #[0x8980fe15, 0x55898ce0, 0x8cf4fbde, 0x5e8537e9, 0x3d2e54f, 0x7e46753f, 0x5d151bb8, 0x2559b733, 0x24560929, 0x6625b1bf, 0xaaccc80e, 0xc5d4287a, 0x2848c46b, 0x94f666c, 0x3adaaeb3, 0x12011250]
 
     let actual := compress cv blockWords counter blockLen flags
     expected == actual
