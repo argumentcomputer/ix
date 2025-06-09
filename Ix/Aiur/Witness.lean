e522960ac9700979c3c00a5afcea9fe065461e76
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2025-06-09 10:36:23 -0300

    wip

--- Ix/Aiur/Witness.lean
+++ Ix/Aiur/Witness.lean
@@ -1,9 +1,18 @@
 import Ix.Aiur.Trace
 import Ix.Aiur.Synthesis
--- import Ix.Archon.Witness
+import Ix.Archon.Circuit
 
 namespace Aiur.Circuit
 
-def populateWitness (circuits : AiurCircuits) (trace : Array Trace) : Archon.Witness := sorry
+def populateWitness (circuits : AiurCircuits) (trace : AiurTrace) : Id Archon.Witness := do
+  let mut witnessModules := #[]
+  let mut heights := #[]
+  for ((mod, _), funcTrace) in circuits.funcs.zip trace.functions do
+    heights := heights.push funcTrace.size.toUInt64
+    witnessModules := witnessModules.push mod.initWitnessModule
+  for ((_, (mod, _)), (_, memTrace)) in circuits.mem.pairs.zip trace.mem do
+    heights := heights.push memTrace.size.toUInt64
+    witnessModules := witnessModules.push mod.initWitnessModule
+  pure $ Archon.compileWitnessModules witnessModules heights
 
 end Aiur.Circuit
