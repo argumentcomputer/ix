545dabc5b8fa224a5ca0bd161d0a4ee8bd1289fe
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2025-06-10 16:47:11 -0400

    Add bench groups and proper performance comparison

--- Benchmarks/Main.lean
+++ Benchmarks/Main.lean
@@ -2,17 +2,19 @@ import Ix.Benchmark
 
 def fib (n : Nat) : Nat :=
   match n with
-  | 0 => 1
+  | 0 => 0
   | 1 => 1
   | n' + 2 => fib (n' + 1) + fib (n')
 
+def fibBench := (bgroup "fib" [
+  bench "fib 1" fib 1,
+  bench "fib 2" fib 2,
+  bench "fib 30" fib 30
+] { numRuns := 100 } )
 
-def fibBench := benchmark [
-  ("fib 1", fib, 1),
-  ("fib 2", fib, 2),
-  ("fib 40", fib, 40)
-]
-
+def addBench := (bgroup "add" [
+  bench "add 1 2" (Nat.add 1) 2
+])
 --def storeBench :=
 --  benchmark []
 --
