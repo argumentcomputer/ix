95820a6270cfa4c866cec3a942b64f4e9f2379be
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2025-11-10 14:19:09 -0300

    ixon datatype

--- Ix/IxVM.lean
+++ Ix/IxVM.lean
@@ -27,6 +27,57 @@ def ixVM := ⟦
     Some([[G; 4]; 8])
   }
 
+  enum Address {
+    Bytes([[G; 4]; 8])
+  }
+
+  enum Ixon {
+    NAnon,                                 -- 0x00, anonymous name
+    NStr(Address, Address),                -- 0x01, string name
+    NNum(Address, Address),                -- 0x02, number name
+    UZero,                                 -- 0x03, universe zero
+    USucc(Address),                        -- 0x04, universe successor
+    UMax(Address, Address),                -- 0x05, universe max
+    UIMax(Address, Address),               -- 0x06, universe impredicative max
+    -- UVar(Nat),                             -- 0x1X, universe variable
+    -- EVar(Nat),                             -- 0x2X, expression variable
+    -- ERef(Address, Vec<Address>),           -- 0x3X, expression reference
+    -- ERec(Nat, Vec<Address>),               -- 0x4X, expression recursion
+    -- EPrj(Address, Nat, Address),           -- 0x5X, expression projection
+    ESort(Address),                        -- 0x80, expression sort
+    EStr(Address),                         -- 0x81, expression string
+    ENat(Address),                         -- 0x82, expression natural
+    EApp(Address, Address),                -- 0x83, expression application
+    ELam(Address, Address),                -- 0x84, expression lambda
+    EAll(Address, Address)                 -- 0x85, expression forall
+    -- ELet(bool, Address, Address, Address), -- 0x86, 0x87, expression let
+    -- Blob(Vec<u8>),                         -- 0x9X, tagged bytes
+    -- Defn(Definition),                      -- 0xA0, definition constant
+    -- Recr(Recursor),                        -- 0xA1, recursor constant
+    -- Axio(Axiom),                           -- 0xA2, axiom constant
+    -- Quot(Quotient),                        -- 0xA3, quotient constant
+    -- CPrj(ConstructorProj),                 -- 0xA4, constructor projection
+    -- RPrj(RecursorProj),                    -- 0xA5, recursor projection
+    -- IPrj(InductiveProj),                   -- 0xA6, inductive projection
+    -- DPrj(DefinitionProj),
+    -- 0xA7, definition projection
+    -- Muts(Vec<MutConst>),                   -- 0xBX, mutual constants
+    -- Prof(Proof),                           -- 0xE0, zero-knowledge proof
+    -- Eval(EvalClaim),                       -- 0xE1, evaluation claim
+    -- Chck(CheckClaim),                      -- 0xE2, typechecking claim
+    -- Comm(Comm),                            -- 0xE3, cryptographic commitment
+    -- Envn(Env),                             -- 0xE4, multi-claim environment
+    -- Prim(BuiltIn),                         -- 0xE5, compiler built-ins
+    -- Meta(Metadata)
+    -- 0xFX, metadata
+  }
+
+  fn serialize(ixon: Ixon) -> ByteStream {
+    match ixon {
+      _ => ByteStream.Nil,
+    }
+  }
+
   fn blake3(input: ByteStream) -> [[G; 4]; 8] {
     let IV = [[103, 230, 9, 106], [133, 174, 103, 187], [114, 243, 110, 60], [58, 245, 79, 165], [127, 82, 14, 81], [140, 104, 5, 155], [171, 217, 131, 31], [25, 205, 224, 91]];
     blake3_compress_layer(blake3_compress_chunks(input, [[0; 4]; 16], 0, 0, [0; 8], IV, Layer.Nil))
