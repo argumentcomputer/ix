10cf623310048ef2672c639fc5039fcfc94f8f3c
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2025-12-05 14:10:10 +0000

    IxVM file split (#278)

---
+++ Ix/IxVM/ByteStream.lean
@@ -0,0 +1,38 @@
+import Ix.Aiur.Meta
+
+namespace IxVM
+
+def byteStream := ⟦
+  enum ByteStream {
+    Cons(G, &ByteStream),
+    Nil
+  }
+
+  fn byte_stream_length(bytes: ByteStream) -> [G; 8] {
+    match bytes {
+      ByteStream.Nil => [0; 8],
+      ByteStream.Cons(_, rest) => relaxed_u64_succ(byte_stream_length(load(rest))),
+    }
+  }
+
+  #[unconstrained]
+  fn read_byte_stream(idx: G, len: G) -> ByteStream {
+    match len {
+      0 => ByteStream.Nil,
+      _ =>
+        let tail = read_byte_stream(idx + 1, len - 1);
+        let [byte] = io_read(idx, 1);
+        ByteStream.Cons(byte, store(tail)),
+    }
+  }
+
+  -- TODO remove this function
+  fn byte_stream_is_empty(input: ByteStream) -> G {
+    match input {
+      ByteStream.Cons(_, _) => 0,
+      ByteStream.Nil => 1,
+    }
+  }
+⟧
+
+end IxVM
