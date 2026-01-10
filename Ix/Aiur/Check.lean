317ea26b2537814037c4df30d68c786cbb8c7658
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2026-01-09 22:38:42 -0300

    Infer term refactor
    into many smaller functions

--- Ix/Aiur/Check.lean
+++ Ix/Aiur/Check.lean
@@ -78,228 +78,43 @@ def refLookup (global : Global) : CheckM Typ := do
 def bindIdents (bindings : List (Local × Typ)) (ctx : CheckContext) : CheckContext :=
   { ctx with varTypes := ctx.varTypes.insertMany bindings }
 
+def fieldTerm (t : TypedTermInner) : TypedTerm := ⟨.field, t, false⟩
+
 mutual
-partial def inferTerm : Term → CheckM TypedTerm
+partial def inferTerm (t : Term) : CheckM TypedTerm := match t with
   | .unit => pure ⟨.unit, .unit, false⟩
-  | .var x => do
-    -- Retrieves and returns the variable type from the context.
-    let ctx ← read
-    match ctx.varTypes[x]? with
-    | some t => pure ⟨t, .var x, false⟩
-    | none =>
-      let Local.str localName := x | unreachable!
-      let typ ← refLookup (Global.init localName)
-      pure ⟨typ, .var x, false⟩
+  | .var x => inferVariable x
   | .ref x => do
-    let typ ← refLookup x
-    pure ⟨typ, .ref x, false⟩
-  | .ret term => do
-    -- Ensures that the type of the returned term matches the expected return type.
-    -- The term is not allowed to have a (nested) return.
-    -- Returning the type of the term is not necessary because it's already in the context.
-    let ctx ← read
-    let inner ← checkNoEscape term ctx.returnType
-    pure ⟨ctx.returnType, inner, true⟩
-  | .data data => do
-    let (typ, inner) ← inferData data
-    pure ⟨typ, inner, false⟩
-  | .let pat expr body => do
-    -- Returns the type of the body, inferred in the context extended with the bound variable type.
-    -- The bound variable is ensured not to escape.
-    let (exprTyp, exprInner) ← inferNoEscape expr
-    let expr' := ⟨exprTyp, exprInner, false⟩
-    let bindings ← checkPattern pat exprTyp
-    let body' ← withReader (bindIdents bindings) (inferTerm body)
-    pure ⟨body'.typ, .let pat expr' body', body'.escapes⟩
+    pure ⟨← refLookup x, .ref x, false⟩
+  | .ret term => inferReturn term
+  | .data data => inferData data
+  | .let pat expr body => inferLet pat expr body
   | .match term branches => inferMatch term branches
-  | .app func@(⟨.str .anonymous unqualifiedFunc⟩) args => do
-    -- Ensures the function exists in the context and that the arguments, which aren't allowed to
-    -- escape, match the function's input types. Returns the function's output type.
-    let ctx ← read
-    match ctx.varTypes[Local.str unqualifiedFunc]? with
-    | some (.function inputs output) => do
-      let args ← checkArgsAndInputs func args inputs
-      pure ⟨output, .app func args, false⟩
-    | some _ => throw $ .notAFunction func
-    | none => match ctx.decls.getByKey func with
-      | some (.function function) => do
-        let args ← checkArgsAndInputs func args (function.inputs.map Prod.snd)
-        pure ⟨function.output, .app func args, false⟩
-      | some (.constructor dataType constr) => do
-        let args ← checkArgsAndInputs func args constr.argTypes
-        pure ⟨.dataType dataType.name, .app func args, false⟩
-      | _ => throw $ .cannotApply func
-  | .app func args => do
-    -- Only checks global map if it is not unqualified
-    let ctx ← read
-    match ctx.decls.getByKey func with
-    | some (.function function) =>
-      let args ← checkArgsAndInputs func args (function.inputs.map Prod.snd)
-      pure ⟨function.output, .app func args, false⟩
-    | some (.constructor dataType constr) =>
-      let args ← checkArgsAndInputs func args constr.argTypes
-      pure ⟨.dataType dataType.name, .app func args, false⟩
-    | _ => throw $ .cannotApply func
-  | .add a b => do
-    let (a, b) ← checkArith a b
-    pure ⟨.field, .add a b, false⟩
-  | .sub a b => do
-    let (a, b) ← checkArith a b
-    pure ⟨.field, .sub a b, false⟩
-  | .mul a b => do
-    let (a, b) ← checkArith a b
-    pure ⟨.field, .mul a b, false⟩
-  | .eqZero a => do
-    let a ← fieldTerm <$> checkNoEscape a .field
-    pure ⟨.field, .eqZero a, false⟩
-  | .proj tup i => do
-    let (typs, tupInner) ← inferTuple tup
-    if h : i < typs.size then
-      let typ := typs[i]
-      let tup := ⟨.tuple typs, tupInner, false⟩
-      pure ⟨typ, .proj tup i, false⟩
-    else
-      throw $ .indexOoB i
-  | .get arr i => do
-    let (typ, n, inner) ← inferArray arr
-    if i ≥ n then
-      throw $ .indexOoB i
-    else
-      let arr := ⟨.array typ n, inner, false⟩
-      pure ⟨typ, .get arr i, false⟩
-  | .slice arr i j => if j < i then throw $ .negativeRange i j else do
-    let (typ, n, inner) ← inferArray arr
-    if j ≤ n then
-      let arr := ⟨.array typ n, inner, false⟩
-      pure ⟨.array typ (j - i), .slice arr i j, false⟩
-    else
-      throw $ .rangeOoB i j
-  | .set arr i val => do
-    let (typ, n, inner) ← inferArray arr
-    if i ≥ n then
-      throw $ .indexOoB i
-    else
-      let val ← checkNoEscape val typ
-      let arrTyp := .array typ n
-      let arr := ⟨arrTyp, inner, false⟩
-      let val := ⟨typ, val, false⟩
-      pure ⟨arrTyp, .set arr i val, false⟩
-  | .store term => do
-    -- Infers the type of the term and returns it, wrapped by a pointer type.
-    -- The term is not allowed to early return.
-    let (typ, inner) ← inferNoEscape term
-    let store := .store ⟨typ, inner, false⟩
-    pure ⟨.pointer typ, store, false⟩
-  | .load term => do
-    -- Ensures that the the type of the term is a pointer type and returns the unwrapped type.
-    -- The term is not allowed to early return.
-    let (typ, inner) ← inferNoEscape term
-    match typ with
-    | .pointer innerTyp =>
-      let load := .load ⟨typ, inner, false⟩
-      pure ⟨innerTyp, load, false⟩
-    | _ => throw $ .notAPointer typ
-  | .ptrVal term => do
-    -- Infers the type of the term, which must be a pointer, but returns `.u64`, as in a cast.
-    -- The term is not allowed to early return.
-    let (typ, inner) ← inferNoEscape term
-    match typ with
-    | .pointer _ => pure $ fieldTerm (.ptrVal ⟨typ, inner, false⟩)
-    | _ => throw $ .notAPointer typ
+  | .app func args => inferApplication func args
   | .ann typ term => do
-    let inner ← checkNoEscape term typ
-    pure ⟨typ, inner, false⟩
-  -- | .unsafeCast term castTyp => do
-  --   let (typ, inner) ← inferNoEscape term
-  --   pure ⟨typ, .unsafeCast inner castTyp, false⟩
-  | .assertEq a b ret => do
-    -- `a` and `b` must have the same type.
-    let (typ, a) ← inferNoEscape a
-    let b ← checkNoEscape b typ
-    let ret ← inferTerm ret
-    let assertEq := .assertEq ⟨typ, a, false⟩ ⟨typ, b, false⟩ ret
-    assert! (ret.escapes == false)
-    pure ⟨ret.typ, assertEq, false⟩
-  | .ioGetInfo key => do
-    let (typ, keyInner) ← inferNoEscape key
-    match typ with
-    | .array .. =>
-      let ioGetInfo := .ioGetInfo ⟨typ, keyInner, false⟩
-      pure ⟨.tuple #[.field, .field], ioGetInfo, false⟩
-    | _ => throw $ .notAnArray typ
-  | .ioSetInfo key idx len ret => do
-    let (keyTyp, keyInner) ← inferNoEscape key
-    match keyTyp with
-    | .array keyEltTyp _ =>
-      if keyEltTyp != .field then throw $ .typeMismatch .field keyEltTyp
-      let idx ← fieldTerm <$> checkNoEscape idx .field
-      let len ← fieldTerm <$> checkNoEscape len .field
-      let ret ← inferTerm ret
-      let ioSetInfo := .ioSetInfo ⟨keyTyp, keyInner, false⟩ idx len ret
-      assert! (ret.escapes == false)
-      pure ⟨ret.typ, ioSetInfo, false⟩
-    | _ => throw $ .notAnArray keyTyp
-  | .ioRead idx len => do
-    if len = 0 then throw .emptyArray
-    let idx ← fieldTerm <$> checkNoEscape idx .field
-    let ioRead := .ioRead idx len
-    pure ⟨.array .field len, ioRead, false⟩
-  | .ioWrite data ret => do
-    let (dataTyp, dataInner) ← inferNoEscape data
-    match dataTyp with
-    | .array dataEltTyp _ =>
-      if dataEltTyp != .field then throw $ .typeMismatch .field dataEltTyp
-      let ret ← inferTerm ret
-      let ioWrite := .ioWrite ⟨dataTyp, dataInner, false⟩ ret
-      assert! (ret.escapes == false)
-      pure ⟨ret.typ, ioWrite, false⟩
-    | _ => throw $ .notAnArray dataTyp
-  | .u8BitDecomposition byte => do
-    let byte ← fieldTerm <$> checkNoEscape byte .field
-    let u8BitDecomposition := .u8BitDecomposition byte
-    pure ⟨.array .field 8, u8BitDecomposition, false⟩
-  | .u8ShiftLeft byte => do
-    let byte ← fieldTerm <$> checkNoEscape byte .field
-    let u8ShiftLeft := .u8ShiftLeft byte
-    pure ⟨.field, u8ShiftLeft, false⟩
-  | .u8ShiftRight byte => do
-    let byte ← fieldTerm <$> checkNoEscape byte .field
-    let u8ShiftRight := .u8ShiftRight byte
-    pure ⟨.field, u8ShiftRight, false⟩
-  | .u8Xor i j => do
-    let i ← fieldTerm <$> checkNoEscape i .field
-    let j ← fieldTerm <$> checkNoEscape j .field
-    let u8Xor := .u8Xor i j
-    pure ⟨.field, u8Xor, false⟩
-  | .u8Add i j => do
-    let i ← fieldTerm <$> checkNoEscape i .field
-    let j ← fieldTerm <$> checkNoEscape j .field
-    let u8Add := .u8Add i j
-    pure ⟨.tuple #[.field, .field], u8Add, false⟩
-  | .debug label term ret => do
-    let term ← term.mapM inferTerm
-    let ret ← inferTerm ret
-    let debug := .debug label term ret
-    assert! (ret.escapes == false)
-    pure ⟨ret.typ, debug, false⟩
-where
-  /--
-  Ensures that there are as many arguments and as expected types and that
-  the types of the arguments are precisely those expected.
-  -/
-  checkArgsAndInputs func args inputs : CheckM (List TypedTerm) := do
-    let lenArgs := args.length
-    let lenInputs := inputs.length
-    unless lenArgs == lenInputs do throw $ .wrongNumArgs func lenArgs lenInputs
-    let pass := fun (arg, input) => do
-      let inner ← checkNoEscape arg input
-      pure ⟨input, inner, false⟩
-    args.zip inputs |>.mapM pass
-  checkArith a b := do
-    let aInner ← checkNoEscape a .field
-    let bInner ← checkNoEscape b .field
-    pure (fieldTerm aInner, fieldTerm bInner)
-  fieldTerm inner := ⟨.field, inner, false⟩
+    pure ⟨typ, ← checkNoEscape term typ, false⟩
+  | .proj tup i => inferProj tup i
+  | .get arr i => inferGet arr i
+  | .slice arr i j => inferSlice arr i j
+  | .set arr i val => inferSet arr i val
+  | .store term => inferStore term
+  | .load term => inferLoad term
+  | .ptrVal term => inferPtrVal term
+  | .eqZero a => inferUnop a .eqZero .field
+  | .add a b => inferBinop a b .add .field
+  | .sub a b => inferBinop a b .sub .field
+  | .mul a b => inferBinop a b .mul .field
+  | .u8ShiftLeft a => inferUnop a .u8ShiftLeft .field
+  | .u8ShiftRight a => inferUnop a .u8ShiftRight .field
+  | .u8BitDecomposition a => inferUnop a .u8BitDecomposition (.array .field 8)
+  | .u8Xor a b => inferBinop a b .u8Xor .field
+  | .u8Add a b => inferBinop a b .u8Add (.tuple #[.field, .field])
+  | .ioGetInfo key => inferIoGetInfo key
+  | .ioSetInfo key idx len ret => inferIoSetInfo key idx len ret
+  | .ioRead idx len => inferIoRead idx len
+  | .ioWrite data ret => inferIoWrite data ret
+  | .assertEq a b ret => inferAssertEq a b ret
+  | .debug label term ret => inferDebug label term ret
 
 partial def checkNoEscape (term : Term) (typ : Typ) : CheckM TypedTermInner := do
   let (typ', inner) ← inferNoEscape term
@@ -311,13 +126,207 @@ partial def inferNoEscape (term : Term) : CheckM (Typ × TypedTermInner) := do
   if typedTerm.escapes then throw .illegalReturn
   pure (typedTerm.typ, typedTerm.inner)
 
-partial def inferData : Data → CheckM (Typ × TypedTermInner)
-  | .field g => pure (.field, .data (.field g))
+partial def inferUnop
+  (a : Term)
+  (op : TypedTerm → TypedTermInner)
+  (typ : Typ) :
+  CheckM TypedTerm := do
+  let a ← fieldTerm <$> checkNoEscape a .field
+  pure ⟨typ, op a, false⟩
+
+partial def inferBinop
+  (a : Term)
+  (b : Term)
+  (op : TypedTerm → TypedTerm → TypedTermInner)
+  (typ : Typ) :
+  CheckM TypedTerm := do
+  let a ← fieldTerm <$> checkNoEscape a .field
+  let b ← fieldTerm <$> checkNoEscape b .field
+  pure ⟨typ, op a b, false⟩
+
+partial def inferProj (tup : Term) (i : Nat) : CheckM TypedTerm := do
+  let (typs, tupInner) ← inferTuple tup
+  if h : i < typs.size then
+    let typ := typs[i]
+    let tup := ⟨.tuple typs, tupInner, false⟩
+    pure ⟨typ, .proj tup i, false⟩
+  else
+    throw $ .indexOoB i
+
+partial def inferGet (arr : Term) (i : Nat) : CheckM TypedTerm := do
+  let (typ, n, inner) ← inferArray arr
+  if i ≥ n then
+    throw $ .indexOoB i
+  else
+    let arr := ⟨.array typ n, inner, false⟩
+    pure ⟨typ, .get arr i, false⟩
+
+partial def inferSlice (arr : Term) (i j : Nat) : CheckM TypedTerm :=
+  if j < i then throw $ .negativeRange i j else do
+    let (typ, n, inner) ← inferArray arr
+    if j ≤ n then
+      let arr := ⟨.array typ n, inner, false⟩
+      pure ⟨.array typ (j - i), .slice arr i j, false⟩
+    else
+      throw $ .rangeOoB i j
+
+partial def inferSet (arr : Term) (i : Nat) (val : Term) : CheckM TypedTerm := do
+  let (typ, n, inner) ← inferArray arr
+  if i ≥ n then
+    throw $ .indexOoB i
+  else
+    let val ← checkNoEscape val typ
+    let arrTyp := .array typ n
+    let arr := ⟨arrTyp, inner, false⟩
+    let val := ⟨typ, val, false⟩
+    pure ⟨arrTyp, .set arr i val, false⟩
+
+partial def inferStore (term : Term) : CheckM TypedTerm := do
+  let (typ, inner) ← inferNoEscape term
+  let store := .store ⟨typ, inner, false⟩
+  pure ⟨.pointer typ, store, false⟩
+
+partial def inferLoad (term : Term) : CheckM TypedTerm := do
+  let (typ, inner) ← inferNoEscape term
+  match typ with
+  | .pointer innerTyp =>
+    let load := .load ⟨typ, inner, false⟩
+    pure ⟨innerTyp, load, false⟩
+  | _ => throw $ .notAPointer typ
+
+partial def inferPtrVal (term : Term) : CheckM TypedTerm := do
+  let (typ, inner) ← inferNoEscape term
+  match typ with
+  | .pointer _ => pure $ fieldTerm (.ptrVal ⟨typ, inner, false⟩)
+  | _ => throw $ .notAPointer typ
+
+partial def inferIoGetInfo (key : Term) : CheckM TypedTerm := do
+  let (typ, keyInner) ← inferNoEscape key
+  match typ with
+  | .array .. =>
+    let ioGetInfo := .ioGetInfo ⟨typ, keyInner, false⟩
+    pure ⟨.tuple #[.field, .field], ioGetInfo, false⟩
+  | _ => throw $ .notAnArray typ
+
+partial def inferIoSetInfo (key idx len ret : Term) : CheckM TypedTerm := do
+  let (keyTyp, keyInner) ← inferNoEscape key
+  match keyTyp with
+  | .array keyEltTyp _ =>
+    if keyEltTyp != .field then throw $ .typeMismatch .field keyEltTyp
+    let idx ← fieldTerm <$> checkNoEscape idx .field
+    let len ← fieldTerm <$> checkNoEscape len .field
+    let ret ← inferTerm ret
+    let ioSetInfo := .ioSetInfo ⟨keyTyp, keyInner, false⟩ idx len ret
+    pure ⟨ret.typ, ioSetInfo, ret.escapes⟩
+  | _ => throw $ .notAnArray keyTyp
+
+partial def inferIoRead (idx : Term) (len : Nat) : CheckM TypedTerm := do
+  if len = 0 then throw .emptyArray
+  let idx ← fieldTerm <$> checkNoEscape idx .field
+  let ioRead := .ioRead idx len
+  pure ⟨.array .field len, ioRead, false⟩
+
+partial def inferIoWrite (data ret : Term) : CheckM TypedTerm := do
+  let (dataTyp, dataInner) ← inferNoEscape data
+  match dataTyp with
+  | .array dataEltTyp _ =>
+    if dataEltTyp != .field then throw $ .typeMismatch .field dataEltTyp
+    let ret ← inferTerm ret
+    let ioWrite := .ioWrite ⟨dataTyp, dataInner, false⟩ ret
+    pure ⟨ret.typ, ioWrite, ret.escapes⟩
+  | _ => throw $ .notAnArray dataTyp
+
+partial def inferVariable (x : Local) : CheckM TypedTerm := do
+  let ctx ← read
+  match ctx.varTypes[x]? with
+  | some t => pure ⟨t, .var x, false⟩
+  | none =>
+    let Local.str localName := x | unreachable!
+    let typ ← refLookup (Global.init localName)
+    pure ⟨typ, .var x, false⟩
+
+partial def inferReturn (term : Term) : CheckM TypedTerm := do
+  let ctx ← read
+  let inner ← checkNoEscape term ctx.returnType
+  pure ⟨ctx.returnType, inner, true⟩
+
+partial def inferLet (pat : Pattern) (expr : Term) (body : Term) : CheckM TypedTerm := do
+  let (exprTyp, exprInner) ← inferNoEscape expr
+  let expr' := ⟨exprTyp, exprInner, false⟩
+  let bindings ← checkPattern pat exprTyp
+  let body' ← withReader (bindIdents bindings) (inferTerm body)
+  pure ⟨body'.typ, .let pat expr' body', body'.escapes⟩
+
+partial def inferUnqualifiedApp (func : Global) (unqualifiedFunc : String) (args : List Term) : CheckM TypedTerm := do
+  let ctx ← read
+  match ctx.varTypes[Local.str unqualifiedFunc]? with
+  | some (.function inputs output) => do
+    let args ← checkArgsAndInputs func args inputs
+    pure ⟨output, .app func args, false⟩
+  | some _ => throw $ .notAFunction func
+  | none => match ctx.decls.getByKey func with
+    | some (.function function) => do
+      let args ← checkArgsAndInputs func args (function.inputs.map Prod.snd)
+      pure ⟨function.output, .app func args, false⟩
+    | some (.constructor dataType constr) => do
+      let args ← checkArgsAndInputs func args constr.argTypes
+      pure ⟨.dataType dataType.name, .app func args, false⟩
+    | _ => throw $ .cannotApply func
+where
+  checkArgsAndInputs func args inputs : CheckM (List TypedTerm) := do
+    let lenArgs := args.length
+    let lenInputs := inputs.length
+    unless lenArgs == lenInputs do throw $ .wrongNumArgs func lenArgs lenInputs
+    let pass := fun (arg, input) => do
+      let inner ← checkNoEscape arg input
+      pure ⟨input, inner, false⟩
+    args.zip inputs |>.mapM pass
+
+partial def inferQualifiedApp (func : Global) (args : List Term) : CheckM TypedTerm := do
+  let ctx ← read
+  match ctx.decls.getByKey func with
+  | some (.function function) =>
+    let args ← checkArgsAndInputs func args (function.inputs.map Prod.snd)
+    pure ⟨function.output, .app func args, false⟩
+  | some (.constructor dataType constr) =>
+    let args ← checkArgsAndInputs func args constr.argTypes
+    pure ⟨.dataType dataType.name, .app func args, false⟩
+  | _ => throw $ .cannotApply func
+where
+  checkArgsAndInputs func args inputs : CheckM (List TypedTerm) := do
+    let lenArgs := args.length
+    let lenInputs := inputs.length
+    unless lenArgs == lenInputs do throw $ .wrongNumArgs func lenArgs lenInputs
+    let pass := fun (arg, input) => do
+      let inner ← checkNoEscape arg input
+      pure ⟨input, inner, false⟩
+    args.zip inputs |>.mapM pass
+
+partial def inferApplication (func : Global) (args : List Term) : CheckM TypedTerm :=
+  match func.toName with
+  | .str .anonymous unqualifiedFunc => inferUnqualifiedApp func unqualifiedFunc args
+  | _ => inferQualifiedApp func args
+
+partial def inferAssertEq (a b ret : Term) : CheckM TypedTerm := do
+  let (typ, a) ← inferNoEscape a
+  let b ← checkNoEscape b typ
+  let ret ← inferTerm ret
+  let assertEq := .assertEq ⟨typ, a, false⟩ ⟨typ, b, false⟩ ret
+  pure ⟨ret.typ, assertEq, ret.escapes⟩
+
+partial def inferDebug (label : String) (term : Option Term) (ret : Term) : CheckM TypedTerm := do
+  let term ← term.mapM inferTerm
+  let ret ← inferTerm ret
+  let debug := .debug label term ret
+  pure ⟨ret.typ, debug, ret.escapes⟩
+
+partial def inferData : Data → CheckM TypedTerm
+  | .field g => pure ⟨.field, .data (.field g), false⟩
   | .tuple terms => do
     let typsAndInners ← terms.mapM inferNoEscape
     let typs := typsAndInners.map Prod.fst
     let terms := typsAndInners.map fun (typ, inner) => ⟨typ, inner, false⟩
-    pure (.tuple typs, .data (.tuple terms))
+    pure ⟨.tuple typs, .data (.tuple terms), false⟩
   | .array terms => do
     if h : terms.size > 0 then
       let (typ, firstInner) ← inferNoEscape terms[0]
@@ -326,7 +335,7 @@ partial def inferData : Data → CheckM (Typ × TypedTermInner)
       for term in terms[1:] do
         let inner ← checkNoEscape term typ
         typedTerms := typedTerms.push ⟨typ, inner, false⟩
-      pure (.array typ terms.size, .data (.array typedTerms))
+      pure ⟨.array typ terms.size, .data (.array typedTerms), false⟩
     else throw .emptyArray
 
 /-- Infers the type of a 'match' expression and ensures its patterns and branches are valid. -/
